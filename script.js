4e7b88d89c1e49983a19250905ff40b937b8edae
Okley <103600535+FTOkley@users.noreply.github.com>
2022-07-12 17:10:52 +0000

    1

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var themeB = document.querySelector("#second");
 var themeC = document.querySelector("#third");
 // var screen = document.querySelector("#display");
 // var answer = document.querySelector(".row");
-var keys = document.querySelectorAll('.key');
-var screen = document.getElementById('display');
+var keys = document.querySelectorAll(".key");
+var screen = document.getElementById("display");
 var backMain = document.getElementsByTagName("body");
 var backToggle = document.getElementsByClassName("switchbox");
 var backKeypad = document.getElementsByClassName("keypads");
@@ -67,32 +67,29 @@ arr.forEach((element, index) => {
   element.addEventListener("click", () => {
     element.style.opacity = "1";
     if (index == 0) {
-
-
       backMain[0].style.backgroundColor = "hsl(222, 26%, 31%)";
       calcBody[0].style.backgroundColor = "hsl(222, 26%, 31%)";
       backToggle[0].style.backgroundColor = "hsl(223, 31%, 20%)";
       backKeypad[0].style.backgroundColor = "hsl(223, 31%, 20%)";
       backKey[0].style.backgroundColor = "hsl(281, 89%, 26%)";
-      console.log( screen.style["backgroundColor"] = "hsl(224, 36%, 15%)");
-      console.log( screen.style["color"] = "hsl(0, 0, 100%)");
-      
-      keys.forEach(key => {
+      console.log((screen.style["backgroundColor"] = "hsl(224, 36%, 15%)"));
+      console.log((screen.style["color"] = "hsl(0, 0, 100%)"));
+
+      keys.forEach((key) => {
         key.style.backgroundColor = "hsl(30, 25%, 89%)";
       });
-      
-      keys.forEach(key => {
+
+      keys.forEach((key) => {
         key.style.color = "hsl(221, 14%, 31%)";
       });
 
-      keys.forEach(key => {
+      keys.forEach((key) => {
         key.style.boxShadow = "0px 5px hsl(28, 16%, 65%)";
       });
-      
+
       answers.style["boxShadow"] = "0 5px hsl(6, 70%, 34%)";
-      console.log( answers.style["backgroundColor"] = "hsl(6, 63%, 50%)");
-      console.log( answers.style["color"] = "hsl(198, 20%, 13%)");
-      
+      console.log((answers.style["backgroundColor"] = "hsl(6, 63%, 50%)"));
+      console.log((answers.style["color"] = "hsl(198, 20%, 13%)"));
 
       reset.style["boxShadow"] = "0 5px hsl(224, 28%, 35%)";
       reset.style["backgroundColor"] = "hsl(225, 21%, 49%)";
@@ -100,8 +97,7 @@ arr.forEach((element, index) => {
       del.style["boxShadow"] = "0 5px hsl(224, 28%, 35%)";
       del.style["backgroundColor"] = "hsl(225, 21%, 49%)";
 
-
-/*
+      /*
       backMain[0].style.backgroundColor = "hsl(222, 26%, 31%)";
       calcBody[0].style.backgroundColor = "hsl(222, 26%, 31%)";
       backToggle[0].style.backgroundColor = "hsl(223, 31%, 20%)";
@@ -149,31 +145,29 @@ arr.forEach((element, index) => {
         "hsl(0, 36%, 15%)";
         */
     } else if (index == 1) {
-      
       backMain[0].style.backgroundColor = "hsl(0, 0%, 90%)";
       calcBody[0].style.backgroundColor = "hsl(0, 0%, 90%)";
       backToggle[0].style.backgroundColor = "hsl(0, 5%, 81%)";
       backKeypad[0].style.backgroundColor = "hsl(0, 5%, 81%)";
       backKey[0].style.backgroundColor = "hsl(281, 89%, 26%)";
-      console.log( screen.style["backgroundColor"] = "hsl(0, 0%, 93%)");
-      console.log( screen.style["color"] = "hsl(60, 10%, 19%)");
-      
-      keys.forEach(key => {
+      console.log((screen.style["backgroundColor"] = "hsl(0, 0%, 93%)"));
+      console.log((screen.style["color"] = "hsl(60, 10%, 19%)"));
+
+      keys.forEach((key) => {
         key.style.backgroundColor = "hsl(45, 7%, 89%)";
       });
-      
-      keys.forEach(key => {
+
+      keys.forEach((key) => {
         key.style.color = "hsl(60, 10%, 19%)";
       });
 
-      keys.forEach(key => {
+      keys.forEach((key) => {
         key.style.boxShadow = "0px 5px hsl(35, 11%, 61%)";
       });
-      
+
       answers.style["boxShadow"] = "0 5px hsl(25, 99%, 27%)";
-      console.log( answers.style["backgroundColor"] = "hsl(25, 98%, 40%)");
-      console.log( answers.style["color"] = "hsl(198, 20%, 13%)");
-      
+      console.log((answers.style["backgroundColor"] = "hsl(25, 98%, 40%)"));
+      console.log((answers.style["color"] = "hsl(198, 20%, 13%)"));
 
       reset.style["boxShadow"] = "0 5px hsl(185, 58%, 25%)";
       reset.style["backgroundColor"] = "hsl(185, 42%, 37%)";
@@ -182,7 +176,7 @@ arr.forEach((element, index) => {
       del.style["backgroundColor"] = "hsl(185, 42%, 37%)";
 
       // document.getElementsByTagName("body")[0].style.backgroundColor ="hsl(0, 0%, 90%)";
-/*
+      /*
         backMain[0].style.backgroundColor = "hsl(0, 0%, 90%)";
         calcBody[0].style.backgroundColor = "hsl(0, 0%, 90%)";
         backToggle[0].style.backgroundColor = "hsl(0, 5%, 81%)";
@@ -216,7 +210,7 @@ arr.forEach((element, index) => {
       // console.log( document.getElementsByTagName("body")[0].style.backgroundColor ="hsl(268, 75%, 9%)");
 
       //backKeypad[0].style.backgroundColor = "hsl(268, 71%, 12%)";
-     
+
       // keys.style["boxShadow"] = "10px 10px hsl(177, 92%, 70%)"
       //  themeC[0].style.color = "hsl(268, 71%, 12%)";
       //  themeC[0].style.opacity = "1";
@@ -225,25 +219,24 @@ arr.forEach((element, index) => {
       backToggle[0].style.backgroundColor = "hsl(268, 71%, 12%)";
       backKeypad[0].style.backgroundColor = "hsl(268, 71%, 12%)";
       backKey[0].style.backgroundColor = "hsl(281, 89%, 26%)";
-      console.log( screen.style["backgroundColor"] = "hsl(268, 71%, 12%");
-      console.log( screen.style["color"] = "hsl(52, 100%, 62%)");
-      
-      keys.forEach(key => {
+      console.log((screen.style["backgroundColor"] = "hsl(268, 71%, 12%"));
+      console.log((screen.style["color"] = "hsl(52, 100%, 62%)"));
+
+      keys.forEach((key) => {
         key.style.backgroundColor = "hsl(268, 47%, 21%)";
       });
-      
-      keys.forEach(key => {
+
+      keys.forEach((key) => {
         key.style.color = "hsl(52, 100%, 62%)";
       });
 
-      keys.forEach(key => {
+      keys.forEach((key) => {
         key.style.boxShadow = "0px 5px hsl(285, 91%, 52%)";
       });
-      
+
       answers.style["boxShadow"] = "0 5px hsl(177, 92%, 70%)";
-      console.log( answers.style["backgroundColor"] = "hsl(176, 100%, 44%)");
-      console.log( answers.style["color"] = "hsl(198, 20%, 13%)");
-      
+      console.log((answers.style["backgroundColor"] = "hsl(176, 100%, 44%)"));
+      console.log((answers.style["color"] = "hsl(198, 20%, 13%)"));
 
       reset.style["boxShadow"] = "0 5px hsl(285, 91%, 52%)";
       reset.style["backgroundColor"] = "hsl(281, 89%, 26%)";
@@ -251,9 +244,9 @@ arr.forEach((element, index) => {
       del.style["boxShadow"] = "0 5px hsl(285, 91%, 52%)";
       del.style["backgroundColor"] = "hsl(281, 89%, 26%)";
 
-    // console.log( text.style.color = "hsl(52, 100%, 62%)");
+      // console.log( text.style.color = "hsl(52, 100%, 62%)");
 
-/*
+      /*
       
       //calculator background
       document.getElementsByClassName("calculator")[0].style.backgroundColor = "hsl(268, 75%, 9%)";
